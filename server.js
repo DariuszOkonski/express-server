fdffdf3d52303033f76f5b8b8373b9deeffb08cd
Dariusz Okoński <darek200180@gmail.com>
2025-08-14 08:27:12 +0200

    prepare public and views folders

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
