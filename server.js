92433b810031aa4c872c6d1143b41912cd42683e
Dariusz Okoński <darek200180@gmail.com>
2025-09-03 08:06:49 +0200

    add main.hbs and navigation

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const hbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', hbs());
+app.engine('hbs', hbs({ extname: 'hbs', defaultLayout: 'main' }));
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/user', (req, res) => {
-  res.render('forbidden', { layout: false });
+  res.render('forbidden');
 });
 
 app.get('/', (req, res) => {
@@ -18,19 +18,31 @@ app.get('/', (req, res) => {
 });
 
 app.get('/home', (req, res) => {
-  res.render('home', { layout: false });
+  res.render('home');
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', { layout: false });
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
 });
 
 app.get('/hello/:name', (req, res) => {
-  res.render('hello', { layout: false, name: req.params.name });
+  res.render('hello', { name: req.params.name });
 });
 
 app.use((req, res) => {
-  res.render('404', { layout: false });
+  res.render('404');
 });
 
 const PORT = 8000;
